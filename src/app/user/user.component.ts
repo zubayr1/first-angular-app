15a3bf045e35ec23deac66747aa2df3ca4ff1714
Zubayr Khalid <zkhalid@audeering.com>
2024-06-05 15:38:07 +0200

    angular first project

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  get imagepath() {
+    return 'assets/users/' + this.selectedUser().avatar;
+  }
+
+  onSelectuser() {
+    randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
+}
